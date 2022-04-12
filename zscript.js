741432662e086fd9ff5c977d48ff5140c950e5cd
faizalr <faizal.abdulkadir@gmail.com>
2022-04-12 23:22:41 +0800

    added some tests

--- zscript.js
+++ zscript.js
@@ -1,4 +1,6 @@
-let dbg_list = ["main: ", "lex: ", "error: "];
+let dbg_list = ["main: ", 
+    //"lex: ", 
+    "error: "];
 Array.prototype.toString = function () {
     return "[" + this.join(", ") + "]";
 };
@@ -10,6 +12,15 @@ function main () {
         def n : 99 + 1 * 2
         def n : 99 + 1 * 2 / 3
         set! n : 100
+        `;
+    test(`def n : 2`, ["def", "n", 2]);
+    test(`def n : 2 + 3`, ["def", "n", ["+", 2, 3]]);
+    test(`def n : 2 + 3 + 4`, ["def", "n", ["+", ["+", 2, 3], 4]]);
+    test(`def n : 2 + 3 * 4`, ["def", "n", ["+", 2, ["*", 3, 4]]]);
+    test(`def n : 2 / 3 * 4`, ["def", "n", ["*", ["/", 2, 3], 4]]);
+    test(`set! n : 2`, ["set!", "n", 2]);
+    test(`2 + 3 > 4 * 5`, [">", ["+", 2, 3], ["*", 4, 5]]);
+    test(`
         def fac : fun (n)
             if n < 2 then
                 n
@@ -17,9 +28,38 @@ function main () {
                 n * fac(n - 1)
             fi
         nuf
-        `;
+        `,
+        ["def", "fac", ["fun", ["n"],
+            ["if", ["<", "n", 2], "n",
+                  ["*", "n", ["fac", ["-", "n", 1]]]]
+        ]]);
+    //let ast = read(code);
+    //dbg("main: ", ast);
+}
+
+function test(code, expected) {
     let ast = read(code);
-    dbg("main: ", ast);
+    if (eq(ast, expected)) {
+        console.log("pass: ", code, " => ", ast);
+    }
+    else {
+        console.log("fail: ", ast, " not= ", expected);
+        throw "bail !";
+    }
+}
+
+function eq (a, b) {
+    if (Array.isArray(a) && Array.isArray(b) && a.length === b.length) {
+        for (let i = 0; i < a.length; i += 1) {
+            if (!eq(a[i], b[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    else {
+        return a === b;
+    }
 }
 
 function read (s) {
@@ -41,40 +81,34 @@ function parse (arr) {
         if (kw.includes(arr[0])) {
             return ret.length === 2 ? ret[1] : ret;
         }
-        ret.push(parse_plus(arr));
+        ret.push(parse_first(arr));
     }
     return ret.length === 2 ? ret[1] : ret;
+}
+
+function parse_first (arr) {
+// aka parse_gt
+    let ret = parse_plus(arr);
+    while (arr[0] === "<" || arr[0] === "<=" ||
+           arr[0] === ">" || arr[0] === ">="  ) {
+        ret = [arr.shift(), ret, parse_plus(arr)];
+    }
+    return ret;
 }
 
 // hierarchy: +|- *|/ var
 function parse_plus (arr) {
     let ret = parse_mult(arr);
-    if (arr[0] === "+" || arr[0] === "-") {
-        while (arr[0] === "+" || arr[0] === "-") {
-            ret = [arr.shift(), ret, parse_mult(arr)];
-        }
+    while (arr[0] === "+" || arr[0] === "-") {
+        ret = [arr.shift(), ret, parse_mult(arr)];
     }
     return ret;
 }
 
 function parse_mult (arr) {
-    let ret = parse_gt(arr);
-    if (arr[0] === "*" || arr[0] === "/") {
-        while (arr[0] === "*" || arr[0] === "/") {
-            ret = [arr.shift(), ret, parse_gt(arr)];
-        }
-    }
-    return ret;
-}
-
-function parse_gt (arr) {
     let ret = parse_var(arr);
-    if (arr[0] === "<" || arr[0] === "<=" ||
-        arr[0] === ">" || arr[0] === ">="  ) {
-        while (arr[0] === "<" || arr[0] === "<=" ||
-               arr[0] === ">" || arr[0] === ">="  ) {
-            ret = [arr.shift(), ret, parse_var(arr)];
-        }
+    while (arr[0] === "*" || arr[0] === "/") {
+        ret = [arr.shift(), ret, parse_var(arr)];
     }
     return ret;
 }
@@ -84,23 +118,23 @@ function parse_var (arr) {
     if (x === "def") {
         let k = arr.shift();
         assert(arr.shift() === ":", "expected :");
-        let v = parse_plus(arr);
+        let v = parse_first(arr);
         return ["def", k, v];
     }
     else if (x === "set!") {
         let k = arr.shift();
         assert(arr.shift() === ":", "expected :");
-        let v = parse_plus(arr);
+        let v = parse_first(arr);
         return ["set!", k, v];
     }
     else if (x === "fun") {
-        let parms = parse_var(arr);
+        let parms = parse_first(arr);
         let body = parse(arr);
         assert(arr.shift() === "nuf", "expected nuf !")
         return ["fun", parms, body];
     }
     else if (x === "if") {
-        let cond = parse_plus(arr);
+        let cond = parse_first(arr);
         assert(arr.shift() === "then", "expected then !");
 
         let conseq = parse(arr);
@@ -117,7 +151,7 @@ function parse_var (arr) {
         return ["if", cond, conseq, alt];
     }
     else if (x === "(") {
-        console.log("parens: arr: " + arr);
+        //console.log("parens: arr: " + arr);
         let ret = [];
         while (1) {
             if (arr.length === 0) {
@@ -128,12 +162,12 @@ function parse_var (arr) {
                 return ret;
             }
             else {
-                ret.push(parse_plus(arr));
+                ret.push(parse_first(arr));
             }
         } // elihw
     }
     else if (!isNaN(x)){
-        return new Number(x);
+        return +x;
     }
     else if (arr[0] === "(") { // fun call ?
         let args = parse_var(arr);
